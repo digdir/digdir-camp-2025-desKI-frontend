7f83f68f75d814edc7a0890a76dcf700c71b8043
Johanne Grønlien Gjedrem <124079917+johannegg@users.noreply.github.com>
2025-06-19 10:54:34 +0200

    Revert "[Bug]: Indent bug in verify.yml (#11)"
    This reverts commit 8321d86dc7c20e5bc23fbbbdded889af505bf982.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './assets/common.css';
-import App from './App.tsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './assets/common.css'
+import App from './App.tsx'
 
 // Importing designsystemet here and only here
 // Import local styles as needed
 import '@digdir/designsystemet-theme';
 import '@digdir/designsystemet-css';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Root element not found');
 
-createRoot(rootElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,

92b65b2ef95bdbfa170ec6e54e0e2622f4a4f9b0
johannegg <johanne.gronlien.gjedrem@digdir.no>
2025-06-27 09:10:49 +0200

    Style: Fix the heading

--- src/Pages/StartPage/StartPage.tsx
+++ src/Pages/StartPage/StartPage.tsx
@@ -1,10 +1,13 @@
 import { logoLight } from '~/assets';
 import styles from './StartPage.module.css';
+
 export function StartPage() {
-return (
-    <div className={styles.mainContainer}>
-        <h1 className={styles.test}>Test</h1>
-        <img src={logoLight} className={styles.logo}/>
-    </div>
- );
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.headerContainer}>
+               <img src={logoLight} className={styles.logo}/>
+               <h2 className={styles.headerQuestion}>Which solution can I help you with today?</h2>
+            </div>
+        </div>
+    );
 }

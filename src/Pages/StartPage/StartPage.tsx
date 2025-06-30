909360297c9e6102ef1845508cbb6a67980976e1
magnus-lower <magnus.lower@gmail.com>
2025-06-30 09:36:38 +0200

    Included comments in the StartPage.tsx file

--- src/Pages/StartPage/StartPage.tsx
+++ src/Pages/StartPage/StartPage.tsx
@@ -2,6 +2,13 @@ import { Card } from '@digdir/designsystemet-react';
 import { logoLight } from '~/assets';
 import styles from './StartPage.module.css';
 
+/*
+  * StartPage component that displays a list of solutions that the user can select to get help with.
+  * Each solution is displayed as a card.
+  * The component is styled using CSS modules.
+  * @returns {JSX.Element} The rendered StartPage component.
+*/
+
 export function StartPage() {
   const solutions = [
     'ID-porten',
@@ -17,6 +24,10 @@ export function StartPage() {
     'Digital lommebok',
     'Other',
   ];
+
+  /*
+    * The main container for the StartPage component.
+  */
   return (
     <div className={styles.mainContainer}>
       <div className={styles.headerContainer}>

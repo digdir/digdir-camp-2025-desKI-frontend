ad6d4b133813d0365e31536f4d11377e85c3c5c6
magnus-lower <magnus.lower@gmail.com>
2025-06-30 09:41:07 +0200

    Included comments in the main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,16 @@ import './styles/common.css';
 import '@digdir/designsystemet-theme';
 import '@digdir/designsystemet-css';
 
+/*
+   * Main entry point for the React application.
+ */
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 
+/*
+   * Render the main application component into the root element.
+   * The application is wrapped in StrictMode for highlighting potential problems in the application.
+ */
 createRoot(rootElement).render(
   <StrictMode>
     <App />

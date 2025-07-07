2d357968812f8ddb9401e2582f39f73cf75bd6c8
johannegg <johanne.gronlien.gjedrem@digdir.no>
2025-07-07 12:42:49 +0200

    Format: run biome

--- src/components/UtilityBar/UtilityBar.tsx
+++ src/components/UtilityBar/UtilityBar.tsx
@@ -1,15 +1,15 @@
-import { Button } from "@digdir/designsystemet-react";
-import { EarthIcon, MoonIcon } from "@navikt/aksel-icons";
-import styles from "./UtilityBar.module.css";
+import { Button } from '@digdir/designsystemet-react';
+import { EarthIcon, MoonIcon } from '@navikt/aksel-icons';
+import styles from './UtilityBar.module.css';
 
 export function UtilityBar() {
   return (
     <div className={styles.utilityBarContainer}>
       <Button className={styles.utilityButton}>
-        <MoonIcon className={styles.moonIcon}/>
+        <MoonIcon className={styles.moonIcon} />
       </Button>
       <Button className={styles.utilityButton}>
-        <EarthIcon/>
+        <EarthIcon />
       </Button>
     </div>
   );

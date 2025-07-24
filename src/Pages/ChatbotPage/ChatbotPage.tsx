ff5ed54ee9f1b1fc5df6502ed264607127ed5f50
johannegg <johanne.gronlien.gjedrem@digdir.no>
2025-07-24 10:08:27 +0200

    feat: Add brukerstotte text instead of dropdown

--- src/Pages/ChatbotPage/ChatbotPage.tsx
+++ src/Pages/ChatbotPage/ChatbotPage.tsx
@@ -121,7 +121,7 @@ export function ChatbotPage({ source }: ChatbotPageProps) {
           <Logo className={styles.logo} />
         </Link>
         {/* Stops dropdown menu from rendering if source is brukerstotte */}
-        {source !== 'brukerstotte' && (
+        {source !== 'brukerstotte' ? (
           <Dropdown.TriggerContext>
             <Dropdown.Trigger
               className={styles.dropdownTrigger}
@@ -144,6 +144,8 @@ export function ChatbotPage({ source }: ChatbotPageProps) {
               </Dropdown.List>
             </Dropdown>
           </Dropdown.TriggerContext>
+        ) : (
+          <h2 className={styles.brukerstotteText}>Brukerstøtte</h2>
         )}
       </div>
 
